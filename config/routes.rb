2232d4d227cd43f43f7cd435081f17c834b62685
Lucy Silver <lucy.silver2@gmail.com>
2020-05-27 16:07:38 +0100

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+get 'about', to: 'pages#about'
+get 'team', to: 'pages#team'
+get 'membership', to: 'pages#membership'
+get 'general', to: 'pages#general'
+get 'cosmetic', to: 'pages#cosmetic'
+get 'facial', to: 'pages#facial'
+get 'contact', to: 'pages#contact'
+get 'covid', to: 'pages#covid'
 end
